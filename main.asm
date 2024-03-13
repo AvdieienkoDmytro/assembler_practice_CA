a3e3dbd875a72e4b8b0771cbc9cbaeadb99fac4a
AvdieienkoDmytro <150156419+AvdieienkoDmytro@users.noreply.github.com>
2024-03-13 20:34:39 +0000

    test commit

--- main.asm
+++ main.asm
@@ -1,28 +1,19 @@
+; test 1
 .model small
-.stack 100h
+
 .data
-    array dw 16 DUP(0) ; Define an array of 15 words, initially filled with 0s
 
 .code
 main PROC
-    mov ax, @data       ; Initialize data segment
-    mov ds, ax
+    MOV AX, 1
+    MOV BX, 0
+    MOV CX, 5
 
-    ; Prepare our loop
-    mov bx, 0           ; Initial position in the array
-    mov cx, 0           ; Loop counter
-    mov ax, 3           ; First element of the array
+    @sum:
+    add bx, ax
+    inc bx
 
-array_loop:
-    ; Use ax and dx as temporary registers to hold the last two Fibonacci numbers
-    mov [array + bx], ax  ; Store F(n) in the array
-    add bx, 2             ; Move to the next position in the array (words)
-    add ax, 3             ; next number in the sequence
-    inc cx
-    cmp cx, 16
-    jne array_loop    ; Repeat if cx != 15
+    loop @sum
 
-    mov ax, 4C00h       ; Terminate program
-    int 21h
 main ENDP
-END main
+end main
